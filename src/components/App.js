e0f2b067e2d3435c8e743bb268c96013032d9a93
Mark Smellie <marksmellie892@gmail.com>
2020-07-07 20:07:09 +0100

    Initial commit

---
+++ src/components/App.js
@@ -0,0 +1,31 @@
+import React from "react";
+import Header from "./Header";
+import Home from "./Home";
+
+class App extends React.Component {
+  state = { selectedPage: "Home", select: "homeSelect" };
+
+  onPageSelect = page => {
+    this.setState({ selectedPage: page });
+    if (page === "Home") {
+      this.setState({ select: "homeSelect" });
+    } else if (page === "About") {
+      this.setState({ select: "aboutSelect" });
+    } else if (page === "Gallery") {
+      this.setState({ select: "gallerySelect" });
+    } else if (page === "CV") {
+      this.setState({ select: "cvSelect" });
+    }
+    console.log("CurrentPage: ", page);
+  };
+
+  render() {
+    return (
+      <div>
+        <Header onPageSelect={this.onPageSelect} select={this.state.select} />
+      </div>
+    );
+  }
+}
+
+export default App;
