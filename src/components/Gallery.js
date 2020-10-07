c4d581c24bf00379a5e33f508dc951f7ae191c89
Mark <marksmellie892@gmail.com>
2020-10-07 17:06:09 +0100

    Updated gallery styling and grid item layout

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -6,21 +6,28 @@ const Gallery = () => {
     <div className="galleryPage">
       <div className="galleryHeader">
         <h1>Gallery</h1>
+        <h3>A collection of some of my work</h3>
       </div>
       <div className="galleryGrid">
         <div className="galleryItem">
+          <div className="galleryItemHeader">Pomodoro Timer</div>
           <div className="galleryItemContent">
-            <span>Description of item 1 that rambles on a bit too long</span>
+            <img src="images/pomodoro.jpg" alt="not found" width="100%" />
+            <div className="galleryItemOverlay">
+              <div className="overlayText">Overlay Text</div>
+            </div>
+          </div>
+        </div>
+        <div className="galleryItem">
+          <div className="galleryItemHeader">Pomodoro Timer</div>
+          <div className="galleryItemContent">
+            <img src="images/pomodoro.jpg" alt="not found" width="100%" />
           </div>
         </div>
         <div className="galleryItem">
           <div className="galleryItemContent">
-            <span>item 2</span>
-          </div>
-        </div>
-        <div className="galleryItem">
-          <div className="galleryItemContent">
-            <span>item 3</span>
+            <div className="galleryItemHeader">Pomodoro Timer</div>
+            <img src="images/pomodoro.jpg" alt="not found" width="100%" />
           </div>
         </div>
         <div className="galleryItem">
