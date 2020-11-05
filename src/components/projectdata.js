f2c1d61eb016f57b153efaf2e6320d026cb6b4c5
Mark <marksmellie892@gmail.com>
2020-11-05 17:47:27 +0000

    Updated project data

--- src/components/projectdata.js
+++ src/components/projectdata.js
@@ -1,48 +1,57 @@
 export const projects = [
     {
+        title: "Video Streaming",
+        overlayText: "A video streaming app in React with Google authentication support.",
+        image: "images/pomodoro.jpg",
+        description: ""
+    },
+    {
         title: "Youtube Search",
-        overlayText: "Overlay text for project 1",
-        image: "images/pomodoro.jpg"
+        overlayText: "A Youtube search app.",
+        image: "images/pomodoro.jpg",
+        description: "An single page application built with React that makes use of the Youtube API. The app allows users to enter a search term that will return a list of videos and allow them to select one to watch."
     },
     {
         title: "Image Search",
-        overlayText: "Overlay text for project 2",
-        image: "images/pomodoro.jpg"
+        overlayText: "An image search app using Unsplash.",
+        image: "images/pomodoro.jpg",
+        description: ""
     },
     {
         title: "Pomodoro Timer",
-        overlayText: "Overlay text for project 3",
-        image: "images/pomodoro.jpg"
-    },
-    {
-        title: "Notes",
-        overlayText: "Overlay text for project 4",
-        image: "images/pomodoro.jpg"
-    },
-    {
-        title: "Widgets",
-        overlayText: "Overlay text for project 5",
-        image: "images/pomodoro.jpg"
+        overlayText: "A Pomodoro timer app in React.",
+        image: "images/pomodoro.jpg",
+        description: ""
     },
     {
         title: "Todo List",
+        overlayText: "A todo list in React.",
+        image: "images/pomodoro.jpg",
+        description: ""
+    },
+    {
+        title: "Widgets",
         overlayText: "Overlay text for project 6",
-        image: "images/pomodoro.jpg"
-    },
-    {
-        title: "Basic Weather",
-        overlayText: "Overlay text for project 7",
-        image: "images/pomodoro.jpg"
-    },
-    {
-        title: "Song List",
-        overlayText: "Overlay text for project 8",
-        image: "images/pomodoro.jpg"
+        image: "images/pomodoro.jpg",
+        description: ""
     },
     {
         title: "Puttered",
+        overlayText: "A web based golf game built using the Phaser framework",
+        image: "images/pomodoro.jpg",
+        description: ""
+    },
+    {
+        title: "Basic Weather",
+        overlayText: "Overlay text for project 8",
+        image: "images/pomodoro.jpg",
+        description: ""
+    },
+    {
+        title: "Song List",
         overlayText: "Overlay text for project 9",
-        image: "images/pomodoro.jpg"
+        image: "images/pomodoro.jpg",
+        description: ""
     }
   ];
   
