9fb25625e16b39d2cc26f7ce322b657e5703142d
marksmellie892@gmail.com <marksmellie892@gmail.com>
2020-11-08 20:34:16 +0000

    Updated project data

--- src/components/projectdata.js
+++ src/components/projectdata.js
@@ -15,19 +15,19 @@ export const projects = [
         title: "Image Search",
         overlayText: "An image search app using Unsplash.",
         image: "images/pomodoro.jpg",
-        description: ""
+        description: "An app built with React that makes use of the Unsplash API to perform image searches. The search query is updated as the user types dynamically sizing results into a grid."
     },
     {
         title: "Pomodoro Timer",
         overlayText: "A Pomodoro timer app in React.",
         image: "images/pomodoro.jpg",
-        description: ""
+        description: "A Pomodoro time app built with React to demonstrate how state works. The app allows the user to start a timer and updates the time left on screen."
     },
     {
         title: "Todo List",
         overlayText: "A todo list in React.",
         image: "images/pomodoro.jpg",
-        description: ""
+        description: "A basic todo list built with React. The app allows users to add items to the list, mark them as complete or remove them from the list."
     },
     {
         title: "Widgets",
@@ -39,19 +39,19 @@ export const projects = [
         title: "Puttered",
         overlayText: "A web based golf game built using the Phaser framework",
         image: "images/pomodoro.jpg",
-        description: ""
+        description: "Puttered is aweb based golf game built using the Phaser framework. The game was developed as part of a mobile web development module at university. The game features multiple levels with online leaderboards to record high scores between users."
     },
     {
         title: "Basic Weather",
         overlayText: "A simple weather app.",
         image: "images/pomodoro.jpg",
-        description: ""
+        description: "A basic React weather app that uses the user location to determine what the weather is likely to be for the user."
     },
     {
         title: "Song List",
         overlayText: "A basic React app to demonstrate how actions and reducers work.",
         image: "images/pomodoro.jpg",
-        description: ""
+        description: "A basic React app built to demonstrate actions and reducers. The app allows users to select a song from the list to see additional details about the selected song."
     }
   ];
   
