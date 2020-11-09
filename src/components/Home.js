d4ec8ab3173b2d69666615e9cf83f31e00845384
Mark <marksmellie892@gmail.com>
2020-11-09 18:42:44 +0000

    Removed TextRoll and update Home text

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,7 +6,7 @@ const Home = () => {
   return (
     <div className="homePage">
       <h1 className="homeIntro">Hi, I'm Mark.</h1>
-      <TextRoll />
+      <h2 className="homeText">I want to be a Web Developer</h2>
       <div className="homeEnding">
         <h3>Let me show you some of my work.</h3>
       </div>
