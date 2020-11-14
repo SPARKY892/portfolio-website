68b3f5373a37eaa2a0a5de96e37333fd34ff1e2d
Mark <marksmellie892@gmail.com>
2020-11-14 21:48:29 +0000

    Updated CV page layout and text

--- src/components/CV.js
+++ src/components/CV.js
@@ -3,13 +3,22 @@ import React from "react";
 
 const Home = () => {
   return (
-    <div className="resumeContainer">
-      <a className="resumeIcon" href="/resume/Mark_Smellie_CV.pdf" download>
-        <i className="far fa-file-pdf"></i>
-      </a>
-      <a className="resumeIcon" href="/resume/Mark_Smellie_CV.docx" download>
-        <i className="far fa-file-word"></i>
-      </a>
+    <div>
+      <h1 className="resumeText">Download a copy of my CV.</h1>
+      <div className="resumeContainer">
+        <div className="iconContainer">
+          <a className="resumeIcon" href="/resume/Mark_Smellie_CV.pdf" download>
+            <i className="far fa-file-pdf"></i>
+          </a>
+          <h1 className="downloadText">PDF</h1>
+        </div>
+        <div className="iconContainer">
+        <a className="resumeIcon" href="/resume/Mark_Smellie_CV.docx" download>
+          <i className="far fa-file-word"></i>
+        </a>
+        <h1 className="downloadText">DOCX</h1>
+        </div>
+      </div>
     </div>
   );
 };
