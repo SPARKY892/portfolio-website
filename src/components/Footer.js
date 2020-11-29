10089406fef171ac304afa474be5ed3607fca923
Mark <marksmellie892@gmail.com>
2020-11-29 11:22:04 +0000

    Updated Footer link rel

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ import React from "react";
 const Footer = () => {
   return (
     <div className="footer">
-      <a className="footerIcon" href="https://github.com/SPARKY892" target="_blank">
+      <a className="footerIcon" href="https://github.com/SPARKY892" target="_blank" rel="noopener noreferrer">
         <i className="fab fa-github"></i>
       </a>
     </div>
