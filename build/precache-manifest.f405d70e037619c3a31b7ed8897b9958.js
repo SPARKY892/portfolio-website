9ec28e3463249ad4bf8f763ba1489845e48628fc
Mark <marksmellie892@gmail.com>
2021-01-19 19:28:06 +0000

    Adding build folder

---
+++ build/precache-manifest.f405d70e037619c3a31b7ed8897b9958.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ebb23ae1c06c834c621e5424f640ba81",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f5db46371b8cc50afa71",
+    "url": "/static/css/main.6c8b4d89.chunk.css"
+  },
+  {
+    "revision": "fd187fb474d7fd0860b2",
+    "url": "/static/js/2.72e4f498.chunk.js"
+  },
+  {
+    "revision": "f5db46371b8cc50afa71",
+    "url": "/static/js/main.2cd08003.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
