6226832319b75141858892b7076b3c2f958ab83c
Mark <marksmellie892@gmail.com>
2021-07-17 15:31:12 +0100

    Removed song list, added route tracker to gallery

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,9 +1,9 @@
 import "./Gallery.scss";
-import React, {useState} from "react";
+import React, { useState } from "react";
 import GalleryItem from "./GalleryItem";
-import Modal from './Modal';
+import Modal from "./Modal";
 
-const Gallery = ({projects}) => {
+const Gallery = ({ projects }) => {
   const [currentItem, setCurrentItem] = useState(0);
   const [toggleModal, setToggleModal] = useState(false);
 
@@ -14,11 +14,14 @@ const Gallery = ({projects}) => {
         title={project.title}
         image={project.image}
         overlayText={project.overlayText}
-        onClick={() => {setCurrentItem(index); setToggleModal(!toggleModal)}}
+        onClick={() => {
+          setCurrentItem(index);
+          setToggleModal(!toggleModal);
+        }}
       />
     );
   });
-  
+
   return (
     <div className="galleryPage">
       <div className="galleryHeader">
@@ -26,9 +29,16 @@ const Gallery = ({projects}) => {
         <h3>A collection of some of my work</h3>
       </div>
       <div className="galleryGrid">
-        <React.Fragment>{renderedItems}</React.Fragment> 
+        <React.Fragment>{renderedItems}</React.Fragment>
       </div>
-      <Modal  title={projects[currentItem].title} description={projects[currentItem].description} onClick={() => setToggleModal(!toggleModal)} modalShown={toggleModal} slideshowImages={projects[currentItem].slideshowImages} githubLink={projects[currentItem].githubLink} />
+      <Modal
+        title={projects[currentItem].title}
+        description={projects[currentItem].description}
+        onClick={() => setToggleModal(!toggleModal)}
+        modalShown={toggleModal}
+        slideshowImages={projects[currentItem].slideshowImages}
+        githubLink={projects[currentItem].githubLink}
+      />
     </div>
   );
 };
