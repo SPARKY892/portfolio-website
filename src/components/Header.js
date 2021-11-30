f50d67ddfe128c5e48fb19875777b85f26f926da
Mark <marksmellie892@gmail.com>
2021-11-30 16:32:07 +0000

    Rename Gallery to Projects

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,29 +1,41 @@
 import "./Header.scss";
 import React from "react";
-import Link from './Link';
+import Link from "./Link";
 
 const Header = ({ onPageSelect, select }) => {
-  return (
-    <div className="header">
-      <div className="navButtons">
-        <ul className="navListWrapper">
-          <li
-            onClick={() => onPageSelect("Home")}
-            className={`home navList ${select}`}
-          >
-            <Link href="/" className="item">Home</Link>
-          </li>
-          <li onClick={() => onPageSelect("Gallery")} className="gallery navList">
-            <Link href="/gallery" className="item">Gallery</Link>
-          </li>
-          <li onClick={() => onPageSelect("CV")} className="cv navList">
-            <Link href="/CV" className="item">CV</Link>
-          </li>
-          <hr className="slidingHR" />
-        </ul>
-      </div>
-    </div>
-  );
+	return (
+		<div className="header">
+			<div className="navButtons">
+				<ul className="navListWrapper">
+					<li
+						onClick={() => onPageSelect("Home")}
+						className={`home navList ${select}`}
+					>
+						<Link href="/" className="item">
+							Home
+						</Link>
+					</li>
+					<li
+						onClick={() => onPageSelect("Projects")}
+						className="projects navList"
+					>
+						<Link href="/projects" className="item">
+							Projects
+						</Link>
+					</li>
+					<li
+						onClick={() => onPageSelect("CV")}
+						className="cv navList"
+					>
+						<Link href="/CV" className="item">
+							CV
+						</Link>
+					</li>
+					<hr className="slidingHR" />
+				</ul>
+			</div>
+		</div>
+	);
 };
 
 export default Header;
--- src/components/projectdata.js
+++ src/components/projectdata.js
@@ -57,58 +57,6 @@ export const projects = [
 		slideshowImages: ["images/youtube.jpg", "images/youtube2.jpg"],
 	},
 	{
-		title: "Image Search",
-		overlayText: "An image search app using Unsplash.",
-		image: "images/imageSearch.jpg",
-		description:
-			"An app built with React that makes use of the Unsplash API to perform image searches. The search query is updated as the user types dynamically sizing results into a grid.",
-		githubLink: "https://github.com/SPARKY892/React-Basic-Image-Search",
-		slideshowImages: [
-			"images/imageSearch.jpg",
-			"images/imageSearch2.jpg",
-			"images/imageSearch3.jpg",
-		],
-	},
-	{
-		title: "Pomodoro Timer",
-		overlayText: "A Pomodoro timer app in React.",
-		image: "images/pomodoro.jpg",
-		description:
-			"A Pomodoro time app built with React to demonstrate how state works. The app allows the user to start a timer and updates the time left on screen.",
-		githubLink: "https://github.com/SPARKY892/React-Pomodoro-Timer",
-		slideshowImages: [
-			"images/pomodoro.jpg",
-			"images/pomodoro2.jpg",
-			"images/pomodoro3.jpg",
-			"images/pomodoro4.jpg",
-			"images/pomodoro5.jpg",
-		],
-	},
-	{
-		title: "Todo List",
-		overlayText: "A todo list in React.",
-		image: "images/todo.jpg",
-		description:
-			"A basic todo list built with React. The app allows users to add items to the list, mark them as complete or remove them from the list.",
-		githubLink: "https://github.com/SPARKY892/React-Todo-List",
-		slideshowImages: ["images/todo.jpg", "images/todo2.jpg"],
-	},
-	{
-		title: "Widgets",
-		overlayText: "A small collection of basic apps including.",
-		image: "images/widgets1.jpg",
-		description:
-			"A collection of widgets offering different functions. \n\nAccordion demonstrates exapndable content sections that will collapse when another section is selected. \n\nThe Search widget makes use of the Wikipedia API to return search results for terms entered by the user. \n\nThe Dropdown widgets demonstrates a drop down menu where the currently selected item is removed from the selections in the dropdown list. \n\nThe Translate widget uses the Google Translate API in order to translate terms entered by the user from English into the language they choose.",
-		githubLink: "https://github.com/SPARKY892/react-widgets",
-		slideshowImages: [
-			"images/widgets1.jpg",
-			"images/widgets2.jpg",
-			"images/widgets3.jpg",
-			"images/widgets4.jpg",
-			"images/widgets5.jpg",
-		],
-	},
-	{
 		title: "Puttered",
 		overlayText: "A web based golf game built using the Phaser framework",
 		image: "images/puttered.jpg",
