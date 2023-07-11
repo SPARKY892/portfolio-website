475bef2feb49346cf749cea5a9e8fdaa2e32887a
Mark <marksmellie892@gmail.com>
2023-07-11 09:49:59 +0100

    Testing changes

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,13 +2,13 @@ import "./Home.scss";
 import React from "react";
 
 const Home = () => {
-	return (
-		<div className="homePage">
-			<h1 className="homeIntro">Hi, I'm Mark</h1>
-			<h2 className="homeText">I'm a Web Developer</h2>
-			<h3 className="homeEnding">Check out some of my projects.</h3>
-		</div>
-	);
+  return (
+    <div className="homePage">
+      <h1 className="homeIntro">Testing</h1>
+      <h2 className="homeText">More Testing</h2>
+      <h3 className="homeEnding">Even More Testing</h3>
+    </div>
+  );
 };
 
 export default Home;
